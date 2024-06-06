9acd16df3e41348c3b7371af9105c95159b51a0d
Henrique Marques <77990915+html-3@users.noreply.github.com>
2024-06-06 01:04:09 -0300

    Bug: navbar only had white background option

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { auth } from '@/utils/firebase';
+import { useTheme } from 'next-themes';
 import { useRouter } from 'next/navigation';
 import { LuCopyPlus, LuLogOut, LuPlusSquare, LuSquareStack, LuUserCircle2 } from 'react-icons/lu';
 
 export default function Navbar() {
   const router = useRouter();
+  const { theme } = useTheme();
 
   return (
-    <div className='fixed bottom-0 bg-inherit z-10 flex justify-around w-full py-5 px-10 bg-white'>
+    <div
+      className={`fixed bottom-0 bg-inherit z-10 flex justify-around w-full py-5 px-10 ${
+        theme == 'dark' ? 'bg-black' : 'bg-white'
+      }`}>
       <button onClick={() => auth.signOut()}>
         <LuLogOut className='w-8 h-8 hover:text-slate-500 transition-colors' />
       </button>
