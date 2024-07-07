d16832f0b2212fd821eaeb27e5f2aff76f9a17b4
Henrique Marques <77990915+html-3@users.noreply.github.com>
2024-07-07 16:15:55 +0200

    Local cache enabled

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, memoryLocalCache } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {localCache: memoryLocalCache()});
 const auth = getAuth(app)
 
 export { app, db, auth }
